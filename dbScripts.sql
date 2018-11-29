c6a7178b92fe225aaf6002c63324e59a32b2bdf8
Lam Tu <lam.tu@xero.com>
2018-11-29 15:56:12 +1100

    [Lam/Lamphai] Modify SQL scripts

--- dbScripts.sql
+++ dbScripts.sql
@@ -1,29 +1,47 @@
 ---------------------- SCENES TABLES -----------------
+/*drop table choices;
 drop table scenes;
+drop procedure addScene;
+drop procedure getScene;
+drop procedure updateScene;
 
-select * from Scenes;
+drop procedure getChoicesFromScene;
+drop procedure updateChoice;
+drop procedure addChoice;
+*/
+
+--select * from Scenes;
 
 create table Scenes(
 	sceneId int identity(1,1),
 	textContent varchar(2500)
 	primary key (sceneId)
 );
+go
+
+create table Choices(
+	choiceId int identity(1,1),
+	precedent int foreign key references Scenes(sceneId),
+	consequent int foreign key references Scenes(sceneId),
+	optionText varchar(1000),
+	primary key (choiceId)
+)
+go
 
 create procedure addScene
 (
 	@content varchar(2500)
 )
 as insert into Scenes values (@content);
+go
 
---addScene 'You woke up on the floor of your room';
---addScene 'The police arrested you game over!';
---addScene 'Find Eleanor things';
 
 create procedure getScene
 (
 	@id int
 )
 as select textContent from Scenes where sceneId = @id;
+go
 
 --getScene 3;
 
@@ -33,19 +51,11 @@ create procedure updateScene
 	@textContent varchar(2500)
 )as update Scenes set textContent = @textContent where sceneId = @id
 go
--- updateScene @textContent = 'Find Eleanor things, cigarette butts, your ring, room keycard', @id = 2
+
 
 -------------------- CHOICES --------------------
 
-select * from Choices;
-
-create table Choices(
-	choiceId int identity(1,1),
-	precedent int foreign key references Scenes(sceneId),
-	consequent int foreign key references Scenes(sceneId),
-	optionText varchar(1000),
-	primary key (choiceId)
-)
+--select * from Choices;
 
 create procedure addChoice
 (
@@ -54,12 +64,9 @@ create procedure addChoice
 	@content varchar(2500)
 )
 as insert into Choices (precedent, consequent, optionText) values (@pre, @cons, @content);
---addChoice 1, 2, 'Look around Dom room';
---go
---addChoice 1, 3, 'Report the murder';
---go
+go
 
-create procedure getChoiceFromScene
+create procedure getChoicesFromScene
 (
 	@id int
 )
@@ -75,5 +82,21 @@ create procedure updateChoice
 	@textContent varchar(2500)
 )as update Choices set optionText = @textContent where precedent = @pre and consequent = @cons;
 go
---updateChoice @textContent = 'Look around the room', @pre = 1, @cons = 2;
+
+addScene 'You woke up on the floor of your room';
+go
+addScene 'The police arrested you game over!';
+go
+addScene 'Find Eleanor things';
+go
+addChoice 1, 2, 'Look around Dom room';
+go
+addChoice 1, 3, 'Report the murder';
+go
+updateScene @textContent = 'Find Eleanor things, cigarette butts, your ring, room keycard', @id = 3
+go
+updateChoice @textContent = 'Look around the room', @pre = 1, @cons = 2;
+go
+
+select * from Scenes
 
