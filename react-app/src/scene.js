3dba51c26546e5c9373f852e4b3c7f56ed938185
Lumi Shrestha <lumi.shrestha@xero.com>
2018-11-29 15:25:04 +1100

    added scene component

---
+++ react-app/src/scene.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Scene extends Component {
+    render() {
+        
+        return (
+         <div> 
+             TITLE
+         </div>
+  
+        );
+    }
+}
+
+export default Scene;
