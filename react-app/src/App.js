20bd52a5e1743fe2213aee1972eeb3a07d7c2143
Zijian Wang <zijian.wang@xero.com>
2018-12-04 13:03:08 +1100

    Fix not auto playback issue

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   async componentDidMount() {
     this.startGame();
-    this.setState({playing: true});
+    setTimeout(this.setState({playing: true}), 1000);
   }  
 
   startGame = () => {
