d2d8ef76a1e2870c192a85eabc0f8fc411511884
Zijian Wang <zijian.wang@xero.com>
2018-12-04 16:16:11 +1100

    make the gameover sound working

---
+++ react-app/src/gameover.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import gameoverSound from './res/game-over-sound-effect.mp3';
+
+class GameOver extends Component {
+
+    changeTheme(){
+        document.getElementsByClassName("gameHeader")[0].style = "background-color: black";
+        document.getElementsByTagName("body")[0].style = "background-color: black";
+        document.getElementsByTagName("div")[0].style = "color: red";
+        console.log('bad ending');
+    }
+    
+    render() {
+        const {endingType} = this.props;
+        if(endingType.trim() === "bad"){
+            this.changeTheme();
+            return(
+                <div>
+                    <audio ref="audio_tag" src={gameoverSound} style={{display:"none"}} controls autoPlay/>
+                    <pre className="gameOver"> 
+    {'  '},--,     .--.           ,---.    .---..-.   .-.,---.  ,---.    <br></br>
+    .' .'     / /\ \ |\    /| | .-'   / .-. )\ \ / / | .-'  | .-.\   <br></br>
+    |  |  __ / /__\ \|(\  / | | `-.   | | |(_)\ V /  | `-.  | `-'/   <br></br>
+    \  \ ( _)|  __  |(_)\/  | | .-'   | | | |  ) /   | .-'  |   (    <br></br>
+    {' '}\  `-) )| |  |)|| \  / | |  `--. \ `-' / (_)    |  `--.| |\ \   <br></br>
+    {' '})\____/ |_|  (_)| |\/| | /( __.'  )---'         /( __.'|_| \)\  <br></br>
+    (__)             '-'  '-'(__)     (_)           (__)        (__) <br></br>
+                    </pre>
+                    </div>
+            );
+        }
+        else if(endingType.trim() === 'good'){
+            return(
+                <div><pre className='theEnd'>
+            _______ .-. .-.,---.    ,---.  .-. .-. ,'|"\   <br/>
+            |__   __||  |||| .-'    | .-'  |  \| | | |\ \  <br/>
+                   {  } )| |   | `-' || `-.    | `-.  |   | | | | \ \ <br/>
+                {   }   (_) |   | .-. || .-'    | .-'  | |\  | | |  \ \   <br/>
+                {  }   | |   | | |)||  `--.  |  `--.| | |)| /(|`-' / <br/>
+                  { }  `-'   /(  (_)/( __.'  /( __.'/(  (_)(__)`--' <br/>
+                    {  }     (__)   (__)     (__)   (__)             <br/>
+                </pre></div>
+            );
+        }
+        else return null;
+    }
+}
+
+export default GameOver;
